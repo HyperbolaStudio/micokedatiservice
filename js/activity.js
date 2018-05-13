5338050ab258120e764572d4e955d84b3a53a383
Crindzebra <crs_16423@outlook.com>
2018-05-13 20:21:30 +0800

    fuck

--- js/activity.js
+++ js/activity.js
@@ -1,6 +1,50 @@
+/*notebook
+*判断登陆状态switch里有2个alert加注释发布版删掉
+*/
+var login;//用户是否登陆
+var todaydate;//远程更新的最新日期（不是本地现在日期）
+var lastdate;//昨日答案解析的日期
+var newest=new Array();//3篇最新文章的路径
+var user={
+	username:"",
+	nickname:""
+}
+var remote={//远程url
+	protocol:"http://",
+	host:"micoke.dashboard.hyperbola.studio",
+};
+var ajax=new XMLHttpRequest();//Ajax object
+var c_obj=JSON.parse(document.cookie);//cookie object
+if(c_obj.username=="" || c_obj.username==null || c_obj.username==undefined){//判断用户是否登陆
+   //没有登陆
+	login=false;
+}else{
+	//本地有登陆记录
+	ajax.open("POST",remote.protocol+remote.host+"/user/login.php",true);
+	var postready={//向远端用来传送验证消息的对象
+		username:c_obj.username,
+		password:c_obj.password
+	};
+	ajax.send(JSON.stringify(postready));//将对象转换为json并发送
+	var receive=JSON.parse(ajax.responseText);//接收到的数据object
+	switch(receive.status){//判断登陆状态
+		case "login_ok":
+			login=true;
+			user.username=c_obj.username;
+			user.nickname=receive.nickname;
+			break;
+		case "login_not_exist":
+			login="false";
+			break;
+		case "login_passed_err":
+			login="false";
+			break;
+		case "login_server_err":
+			login="false";
+			//alert("服务器异常");
+			break;
+		default:
+			//alert("server_no_ack：在登录时服务器没有响应")
+	}
+}
 
-$(document).ready(function(){
-	$("#extendtoday").click(function(){
-		$("#exam").toggle(100);
-	});
-});
