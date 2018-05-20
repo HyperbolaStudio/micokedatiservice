d0425c712c8392f68689f736929520778d21a0bc
Crindzebra <crs_16423@outlook.com>
2018-05-20 20:28:01 +0800

    aa

--- js/activity.js
+++ js/activity.js
@@ -3,10 +3,13 @@
 *判断登陆状态switch里有2个alert加注释发布版删掉
 *判断用户最终是否登陆switch还没写完(要结合dom加元素了)
 */
-var login;//用户是否登陆
+function byID(a){
+	return document.getElementById(a)
+};
+var login=false;//用户是否登陆
 var todaydate;//远程更新的最新日期（不是本地现在日期）
 var lastdate;//昨日答案解析的日期
-var newest=new Array();//3篇最新文章的路径
+var newest[]=new Array(3);//3篇最新文章的路径
 var user={
 	username:"",
 	nickname:""
@@ -16,7 +19,10 @@ var remote={//远程url
 	host:"micoke.dashboard.hyperbola.studio",
 };
 var ajax=new XMLHttpRequest();//Ajax object
-var c_obj;//cookie object
+var c_obj={
+	username:"",
+	password:""
+};//cookie object
 //c_obj codes
 if(c_obj.username=="" || c_obj.username==null || c_obj.username==undefined){//判断用户是否登陆
    //没有登陆
@@ -50,11 +56,14 @@ if(c_obj.username=="" || c_obj.username==null || c_obj.username==undefined){//�
 			//alert("server_no_ack：在登录时服务器没有响应");
 	}
 };
+login=true;
 switch(login){//判断用户最终是否登陆
 	case true:
-		
+		document.getElementById("login").style="display:none";
+		document.getElementbyID("userbox").style="display:block";
 		break;
 	case false:
-		
+		document.getElementbyID("login").style="display:block";
+		document.getElementById("userbox").style="display:false";
 		break;
 };
